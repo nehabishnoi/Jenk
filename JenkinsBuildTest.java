e59ddd75ee17c96219a00e0300ef889cbf580f96
nehabishnoi <nehabishnoi_23@yahoo.co.in>
2018-10-20 23:37:32 +0530

    Added First project to jenkins

---
+++ JenkinsBuildTest.java
@@ -0,0 +1,11 @@
+package com.jenkinstestproject;
+
+public class JenkinsBuildTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Jenkin Build is in progress");
+		
+	}
+
+}
